a5bcfc4bc00a0c92368dc543eb5f50d3ff2e786a
evan0626ma <yuchen0626ma@gmail.com>
2023-02-21 11:42:31 +1100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,9 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
+  app.setGlobalPrefix("api");
   await app.listen(process.env.SERVER_PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  
 }
 bootstrap();
