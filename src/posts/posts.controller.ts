7443ac2b9d7be96851561776aa85924fe024ebec
Taiyi Su <TaiyiSu98@gmail.com>
2023-02-27 13:26:37 +1100

    waterfall backend done

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,107 +1,113 @@
-
-import { HttpStatus } from '@nestjs/common/enums';
-import { Posts } from './schema/post.schema';
-import {PostsService } from './posts.service';
+import { HttpStatus } from "@nestjs/common/enums";
+import { Posts } from "./schema/post.schema";
+import { PostsService } from "./posts.service";
 import {
   Controller,
   Get,
   Post,
   Body,
+  Query,
   Patch,
   Param,
   Delete,
   HttpException,
   NotFoundException,
 } from "@nestjs/common";
-import { Req, Res } from '@nestjs/common';
-import { PostDTO } from './dto/post.dto';
-import { Observable } from 'rxjs';
+import { Req, Res } from "@nestjs/common";
+import { PostDTO } from "./dto/post.dto";
+import { Observable } from "rxjs";
 
-import Image from './interface/Image.interface'
+import Image from "./interface/Image.interface";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-import { FileInterceptor } from '@nestjs/platform-express';
-import { UseInterceptors, UploadedFile } from '@nestjs/common'
-import { diskStorage } from 'multer';
-import { UseGuards } from '@nestjs/common/decorators';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
+import { FileInterceptor } from "@nestjs/platform-express";
+import { UseInterceptors, UploadedFile } from "@nestjs/common";
+import { diskStorage } from "multer";
+import { UseGuards } from "@nestjs/common/decorators";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guards";
 
-import * as path from 'path'
-import {Request} from "express"
+import * as path from "path";
+import { Request } from "express";
+import { Query as ExpressQuery } from "express-serve-static-core";
+import { join } from "path";
 // var path = require('path')
 
-
-
-@Controller('posts')
+@Controller("posts")
 export class PostsController {
-  constructor(private readonly PostsService: PostsService,
-  ) { }
-    
- 
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file', {
+  constructor(private readonly PostsService: PostsService) {}
+
+  @Post("upload")
+  @UseInterceptors(
+    FileInterceptor("file", {
       // limits: {
       // fileSize: +process.env.MAX_FILE_SIZE,
-      
+
       // },
-  fileFilter: (req: any, file: any, cb: any) => {
-          if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
-              // Allow storage of file
-              cb(null, true);
-          } else {
-              // Reject file
-              cb(new HttpException(`Unsupported file type ${path.extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false);
-          }
+      fileFilter: (req: any, file: any, cb: any) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+          // Allow storage of file
+          cb(null, true);
+        } else {
+          // Reject file
+          cb(
+            new HttpException(
+              `Unsupported file type ${path.extname(file.originalname)}`,
+              HttpStatus.BAD_REQUEST
+            ),
+            false
+          );
+        }
       },
-  storage: diskStorage({
-  destination: './files',
-      filename: (req, file, callback) => {
-      const uniqueSufix = uuidv4();
-      // const uniqueSufix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      const ext = path.extname(file.originalname);
-      const filename = `${uniqueSufix}${ext}`;
-      callback(null,`${filename}`)
-        
-      }
-    
-  })}))
-  async upload(@UploadedFile() file, @Res() res): Promise<Image>{
+      storage: diskStorage({
+        destination: "./files",
+        filename: (req, file, callback) => {
+          const uniqueSufix = uuidv4();
+          // const uniqueSufix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = path.extname(file.originalname);
+          const filename = `${uniqueSufix}${ext}`;
+          callback(null, `${filename}`);
+        },
+      }),
+    })
+  )
+  async upload(@UploadedFile() file, @Res() res): Promise<Image> {
     console.log(file);
-    
-  return res.status(HttpStatus.OK).json({
-    message: "Confirmed ",
-    filename: file.filename,
-    path: file.path
-  })
-}
 
-@Get(':imagename')
-findImage(@Param('imagename') imagename, @Res() res): Observable<Image>{
-  return(res.sendFile(path.join(process.cwd(),'./files/' + imagename)))
-}
-  
-  
- @UseGuards(JwtAuthGuard)
- @Post('sent')
- async create(@Body() PostDTO: PostDTO, @Req() req: any) {
-   const posts= new Posts()
-   posts.userEmail = req.user["email"];
-   posts.filename = PostDTO.filename;
-   posts.path = PostDTO.path;
-   posts.tag = PostDTO.tag;
-   posts.price = PostDTO.price;
-
-   await this.PostsService.create(posts)
- 
-   
-   return posts
-    
-   }
+    return res.status(HttpStatus.OK).json({
+      message: "Confirmed ",
+      filename: file.filename,
+      path: file.path,
+    });
   }
-  
 
-  
-  
-  
- 
+  @Get(":imagename")
+  findImage(@Param("imagename") imagename, @Res() res): Observable<Image> {
+    return res.sendFile(path.join(process.cwd(), "./files/" + imagename));
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post("sent")
+  async create(@Body() PostDTO: PostDTO, @Req() req: any) {
+    const posts = new Posts();
+    posts.userEmail = req.user["email"];
+    posts.filename = PostDTO.filename;
+    posts.path = PostDTO.path;
+    posts.tag = PostDTO.tag;
+    posts.price = PostDTO.price;
+
+    await this.PostsService.create(posts);
+
+    return posts;
+  }
+
+  @Get()
+  getAllPosts(@Query() query: ExpressQuery): Promise<Posts[]> {
+    return this.PostsService.findAllPosts(query);
+  }
+
+  @Get("files/:imageName")
+  getPhotos(@Param("imageName") imageName, @Res() response): Promise<unknown> {
+    return response.sendFile(join(process.cwd(), "files" + imageName));
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,50 +1,52 @@
-
-import { ConflictException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common'
-import { Posts, PostDocument } from './schema/post.schema';
-import { Observable } from 'rxjs';
-import { UserService } from 'src/user/user.service';
-import { PostDTO } from './dto/post.dto';
+import { ConflictException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Posts, PostDocument } from "./schema/post.schema";
+import { Observable } from "rxjs";
+import { UserService } from "src/user/user.service";
+import { PostDTO } from "./dto/post.dto";
 import { User, UserDocument } from "../user/schemas/user.schema";
+import { Query } from "express-serve-static-core";
 
 // const slugify = require(slugify)
 
 @Injectable()
 export class PostsService {
+  constructor(
+    @InjectModel(Posts.name) private readonly postModel: Model<PostDocument>,
+    private userService: UserService
+  ) {}
 
-    constructor(
-        @InjectModel(Posts.name) private readonly postModel: Model<PostDocument>,
-        private userService: UserService
+  // public async create(UserEmail:string, PostDTO: PostDTO){
+  //     const post = await this.postModel.create(PostDTO, User)
+  //     return post
+  //    const user  = await this.postModel.findOne({email: UserEmail})
 
-    ) { }
+  //     const post = new this.postModel(PostDTO, {email: UserEmail})
 
+  //     return post.save();
 
+  // }
 
+  public async create(posts: Posts) {
+    const newPosts = new this.postModel(posts);
+    return newPosts.save();
+  }
 
-    // public async create(UserEmail:string, PostDTO: PostDTO){
-    //     const post = await this.postModel.create(PostDTO, User)
-    //     return post
-    //    const user  = await this.postModel.findOne({email: UserEmail})
-        
-    //     const post = new this.postModel(PostDTO, {email: UserEmail})
-        
-    //     return post.save();
-        
-    // }
+  async findAllPosts(query: Query): Promise<Posts[]> {
+    const resPerPage = Number(query.limit);
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
 
-    public async create(posts: Posts) {
-        const newPosts = new this.postModel(posts);
-        return newPosts.save()
-
-
-
-
-
-
-
-    
-    }
-    
+    return this.postModel
+      .find()
+      .sort({ createdAt: "desc" })
+      .limit(resPerPage)
+      .skip(skip);
+  }
 }
