d8831601fcf3f2b610e9d3d9abade39b79276de0
Cindy088 <elisabeth08@163.com>
2023-03-04 12:58:55 +1100

    postman test passed

--- src/collect/collect.module.ts
+++ src/collect/collect.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { CollectService } from "./collect.service";
 import { CollectController } from "./collect.controller";
 import { Collect, CollectSchema } from "./schemas/collect.schema";
-import { Posts, PostSchema } from "src/posts/schema/post.schema";
+import { Posts, PostSchema } from "src/collect/schemas/post.schema";
 
 @Module({
   imports: [
