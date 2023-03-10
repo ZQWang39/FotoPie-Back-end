a497424925265c638ed9aa9f6a65d3ca2090e08c
Adamyyyy <happyadam2008@gmail.com>
2023-03-10 15:15:49 +1100

    fix: fix bug - import Express

--- src/editUser/editUser.controller.ts
+++ src/editUser/editUser.controller.ts
@@ -1,6 +1,5 @@
-
 import { FileInterceptor } from "@nestjs/platform-express";
-import { Request, Response } from "express";
+import { Request, Response, Express } from "express";
 import * as multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import * as sharp from "sharp";
