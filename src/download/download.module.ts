7e86b05114ddc0639b3eda1ac2864d03c016f490
Cindy088 <elisabeth08@163.com>
2023-03-19 18:55:16 +1100

    presigned url generated

--- src/download/download.module.ts
+++ src/download/download.module.ts
@@ -1,13 +1,8 @@
 import { Module } from "@nestjs/common";
-// import { MongooseModule } from "@nestjs/mongoose";
-// import { Posts, PostSchema } from "./schema/post.schema";
 import { DownloadController } from "./download.controller";
 import { DownloadService } from "./download.service";
 
 @Module({
-  // imports: [
-  //   MongooseModule.forFeature([{ name: Posts.name, schema: PostSchema }]),
-  // ],
   controllers: [DownloadController],
   providers: [DownloadService],
   exports: [DownloadService],
