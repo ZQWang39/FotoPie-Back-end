10b642b926d66e941ff7aef9bfd7b62f64aa9175
Jeremy Liu <unswmercury@gmail.com>
2023-04-04 16:23:24 +1000

    Git reset test 1

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,28 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Posts } from "../posts/schema/post.schema";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 import { Query } from "express-serve-static-core";
+import { Client} from "@elastic/elasticsearch";
+import * as natural from "natural";
+import * as stopword from "stopword";
 
 @Injectable()
 export class SearchService {
-    constructor(
-        @InjectModel(Posts.name) private readonly postsModel: Model<Posts>,
-    ) {}
-    async getSearchPostsIdByTag(
-        tag: string,
-        query: Query
-      ): Promise<Posts[]> {
-        const resPerPage = Number(query.limit);
-        const currentPage = Number(query.page) || 1;
-        const skip = resPerPage * (currentPage - 1);
-        return this.postsModel
-          .find({ tag })
-          .sort({ createdAt: "desc" })
-          .limit(resPerPage)
-          .skip(skip);
-      }
+  constructor(
+    @InjectModel(Posts.name) private readonly postsModel: Model<Posts>
+  ) {}
+  async getSearchPostsIdByTag(tag: string, query: Query): Promise<Posts[]> {
+    const resPerPage = Number(query.limit);
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    //const regex = new RegExp(tag, "i");
+
+    const tokenizer = new natural.WordTokenizer();
+    const stemmer = natural.PorterStemmer;
+    const stopwords = stopword.removeStopwords(tokenizer.tokenize(tag));
+
+    const stemmedTerms = stopwords.map((term) => stemmer.stem(term));
+    const searchText = stemmedTerms.join(" ");
+
+    const terms = tag.trim().split(/\s+/);
+    const regex = new RegExp(terms.join("|"), "i");
+
+    // const result = this.postsModel
+    //   .find({
+    //     $or: [
+    //       { $text: { $search: tag } },
+    //       { tags: { $in: tag.split(" ") } },
+    //       { description: { $regex: regex } },
+    //       { score: { $meta: "textScore" } },
+    //     ],
+    //   })
+    //   .sort({ createdAt: "desc" })
+    //   .limit(resPerPage)
+    //   .sort({ score: { $meta: "textScore" } })
+    //   .skip(skip);
+    const result1 = this.postsModel
+      .find(
+        { description: { $regex: regex } }
+        //{ score: { $meta: "textScore" } }
+      )
+      .sort({ createdAt: "desc" })
+      .limit(resPerPage)
+      //.sort({ score: { $meta: "textScore" } })
+      .skip(skip);
+    //console.log(result1, "debug result1");
+    const result2 = this.postsModel
+      .find(
+        {
+          $text: { $search: searchText },
+          tags: { $in: tag.split(" ") },
+        },
+        { score: { $meta: "textScore" } }
+      )
+      .sort({ createdAt: "desc" })
+      .limit(resPerPage)
+      .sort({ score: { $meta: "textScore" } })
+      .skip(skip);
+    //console.log(result2, "debug result2");
+    const combinedResults = await Promise.all([result1, result2]);
+    
+    const mergedResults = [...combinedResults[0], ...combinedResults[1]];
+    //const result = mergedResults;
+    const uniqueResults = new Set(
+      mergedResults.map((post) => post._id.toString())
+    );
+    const result = Array.from(uniqueResults).map((postId) =>
+      mergedResults.find((post) => post._id.toString() === postId)
+    );
 
 
-
+    if (!result) {
+      throw new NotFoundException("Searched posts not found");
+    }
+    return result;
+  }
 }
